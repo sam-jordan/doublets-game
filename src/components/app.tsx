d53175f434896e26ce9d8fe1cfde3fbe657b4feb
sam-jordan <samjjordan24@gmail.com>
2026-02-13 14:08:48 +0000

    Add an App component, move old files into logic

---
+++ src/components/app.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export default function App() {
+    const [words, setWords] = useState([]);
+
+    return <div>
+        <h1>Doublets</h1>
+    </div>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './components/app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <p>Hello world!</p>
+    <App />
   </StrictMode>,
 )
