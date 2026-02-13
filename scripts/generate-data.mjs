6b3939b875ba7d440e4adb876ac8e59e245ef55f
sam-jordan <samjjordan24@gmail.com>
2026-02-13 15:13:50 +0000

    Add function to find the valid words created by changing one letter for every word in the list

--- scripts/generate-data.mjs
+++ scripts/generate-data.mjs
@@ -3,10 +3,32 @@
 import fs from 'node:fs';
 import { EOL } from 'node:os';
 
+const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+
 function generateData() {
     const words = fs.readFileSync('./scripts/five-letter-words.txt', 'utf-8').split(EOL).map(word => word.toUpperCase());
+    const wordLinkMapping = new Map();
 
-    console.log(words)
+    for (const word of words) {
+        const wordAsArray = word.split('');
+        const linkedWords = [];
+        
+        for (let i = 0; i < wordAsArray.length; i++) {
+            for (let j = 0; j < LETTERS.length; j++) {
+                const updatedWordAsArray = [...wordAsArray]
+                updatedWordAsArray[i] = LETTERS[j];
+                const updatedWordAsString = updatedWordAsArray.join('');
+                if (words.includes(updatedWordAsString) && !linkedWords.includes(updatedWordAsString) && updatedWordAsString !== word) {
+                    linkedWords.push(updatedWordAsString);
+                }
+            }
+        }
+
+        wordLinkMapping.set(word, linkedWords);
+        console.log(`${words.indexOf(word)} / ${words.length}`)
+    }
+
+    console.log(wordLinkMapping);
 }
 
 generateData();
