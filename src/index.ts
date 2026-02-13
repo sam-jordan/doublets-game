abf0ef94d578c34db08aa2b914851d3ee24b097b
sam-jordan <samjjordan24@gmail.com>
2026-02-13 12:26:06 +0000

    Add guess functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+import { input } from '@inquirer/prompts';
+
+const game = ['BEST', 'GOAL'];
+
+async function guess() {
+    const answer = await input({ message: '' });
+
+    const response = await fetch(`https://freedictionaryapi.com/api/v1/entries/en/${answer}`);
+
+    if ((await response.json()).entries) {
+        game.push(answer);
+    }
+}
+
+function main() {
+    console.log('-'.repeat(32));
+    console.log(`${game[0]} => ${game[1]}`);
+    console.log('-'.repeat(32));
+    guess();
+}
+
+main();
