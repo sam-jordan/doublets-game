5453cca1ec14261018dbecc66afa9a17d0325ac0
sam-jordan <samjjordan24@gmail.com>
2026-03-13 12:36:50 +0000

    Add a Letter component, refactor Word to use it

---
+++ src/components/letter.tsx
@@ -0,0 +1,3 @@
+export default function Letter(props: {character: string}) {
+    return <div>{props.character}</div>;
+}
--- src/components/word.tsx
+++ src/components/word.tsx
@@ -1,16 +1,9 @@
+import Letter from "./letter";
+
 export default function Word(props: { word: string[], setWord: ((value: string) => void) | undefined }) {
     return (
-        <div>
-            <input 
-                type="text"
-                value={props.word.join('')}
-                onChange={(e) => {
-                    if (props.setWord) {
-                        return props.setWord((e.target.value).toUpperCase());
-                    } 
-                    return;
-                }}
-            />
+        <div tabIndex={0}>
+            {props.word.map(character => <Letter character={character} />)}
         </div>
     );
 } 
