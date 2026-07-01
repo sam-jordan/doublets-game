a800bd24867a91e7b1fbbe3c2a5c643bdb07bf3d
sam-jordan <samjjordan24@gmail.com>
2026-07-01 14:23:52 +0100

    Update lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const lintstagedConfig = {
-    '*.{js,ts,tsx}': ['xo --fix', 'prettier --write'],
-    'package.json': ['prettier --write', 'sort-package-json'],
+    '*.{js,ts,tsx}': ['xo --fix', 'prettier . --write'],
+    'package.json': ['prettier . --write', 'sort-package-json'],
 };
 
 export default lintstagedConfig;
