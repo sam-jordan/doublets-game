4e2909ab12e1accc2546feb04dd6f7c894ca6b5e
sam-jordan <samjjordan24@gmail.com>
2026-07-01 14:16:24 +0100

    Add lint-staged and sort-package-json

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+const lintstagedConfig = {
+    '*.{js,ts,tsx}': ['xo --fix', 'prettier --write'],
+    'package.json': ['prettier --write', 'sort-package-json'],
+};
+
+export default lintstagedConfig;
