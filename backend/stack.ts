66344e913b27608b14362b0e6602e6e6d790a730
sam-jordan <samjjordan24@gmail.com>
2026-07-31 15:49:10 +0100

    Add an S3 bucket to host the game

--- backend/stack.ts
+++ backend/stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 
 export class Stack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props: cdk.StackProps) {
         super(scope, id, props);
 
-        console.log('Hello, AWS!');
+        const bucket = new s3.Bucket(this, 'bucket', {
+            bucketName: 'doublets-game-static-bucket',
+            // AccessControl: s3.BucketAccessControl.PRIVATE,
+            // enforceSSL: true,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+            websiteIndexDocument: 'index.html',
+        });
+
+        new BucketDeployment(this, 'bucket-deployment', {
+            destinationBucket: bucket,
+            sources: [Source.asset('./frontend/build')],
+        });
     }
 }
