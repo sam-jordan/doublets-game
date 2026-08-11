c9e5ccd8ef2231465ad454a204b45911218a3d9c
sam-jordan <samjjordan24@gmail.com>
2026-08-11 11:52:29 +0100

    Add tests for emptyGuesses

---
+++ frontend/src/logic/empty-guesses.test.ts
@@ -0,0 +1,228 @@
+import { describe, expect, it } from 'vitest';
+import { emptyGuess, emptyGuesses } from './empty-guesses';
+import { WordTypes } from './types';
+
+describe('emptyGuess', () => {
+    it('should return an empty guess object', () => {
+        expect(emptyGuess(1)).toStrictEqual({
+            index: 1,
+            letters: [' ', ' ', ' ', ' ', ' '],
+            type: WordTypes.GUESS,
+            changed: [],
+            gameWin: undefined,
+        });
+    });
+});
+
+describe('emptyGuesses', () => {
+    it('should return an array of empty guesses for each difficulty', () => {
+        const empties = emptyGuesses();
+
+        expect(empties.length).toBe(3);
+        expect(empties).toMatchInlineSnapshot(`
+          [
+            [
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 0,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 1,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 2,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 3,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+            ],
+            [
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 0,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 1,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 2,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 3,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 4,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+            ],
+            [
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 0,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 1,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 2,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 3,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 4,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+              {
+                "changed": [],
+                "gameWin": undefined,
+                "index": 5,
+                "letters": [
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                  " ",
+                ],
+                "type": 1,
+              },
+            ],
+          ]
+        `);
+    });
+});
