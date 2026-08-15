04cb94098ec309dff6f9e3e69089162557001abf
sam-jordan <samjjordan24@gmail.com>
2026-08-15 17:00:39 +0300

    Significantly improve login page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,90 +1,86 @@
+import clsx from 'clsx';
 import { useState } from 'react';
 
 export default function Login() {
-    const [type, setType] = useState<'options' | 'signup' | 'login'>('options');
+    const [type, setType] = useState<'empty' | 'signup' | 'login'>('empty');
 
     function getMainSection() {
         switch (type) {
-            case 'options': {
-                return (
-                    <div className='font-(family-name:--standard-fonts) flex flex-col gap-1'>
-                        <button
-                            className='border-2 border-white w-48 cursor-pointer py-2 hover:bg-grey-mid active:bg-grey-mid rounded-3xl'
-                            type='button'
-                            onClick={() => {
-                                setType('login');
-                            }}
-                        >
-                            Log in
-                        </button>
-                        <button
-                            className='border-2 border-white w-48 cursor-pointer py-2 hover:bg-grey-mid active:bg-grey-mid rounded-3xl'
-                            type='button'
-                            onClick={() => {
-                                setType('signup');
-                            }}
-                        >
-                            Sign up
-                        </button>
-                    </div>
-                );
+            case 'empty': {
+                return null;
             }
 
             case 'signup': {
                 return (
-                    <div className='font-(family-name:--standard-fonts)'>
-                        <form className='flex flex-col gap-1'>
-                            <label htmlFor='username'>Username</label>
+                    <>
+                        <div>
+                            <label htmlFor='username' className='block mb-1'>
+                                Username
+                            </label>
                             <input
-                                className='bg-white text-black rounded-xl pl-2 py-1'
+                                className='bg-white text-black rounded-xl pl-2 py-1 focus:border-pink-bright'
                                 id='username'
                             />
-                            <label htmlFor='username'>Password</label>
+                        </div>
+                        <div>
+                            <label htmlFor='username' className='block mb-1'>
+                                Password
+                            </label>
                             <input
                                 className='bg-white text-black rounded-xl pl-2 py-1'
                                 id='password'
                                 type='password'
                             />
-                            <label htmlFor='username'>Confirm password</label>
+                        </div>
+                        <div>
+                            <label htmlFor='username' className='block mb-1'>
+                                Confirm password
+                            </label>
                             <input
                                 className='bg-white text-black rounded-xl pl-2 py-1'
                                 id='confirm'
                                 type='password'
                             />
-                            <button
-                                className='border-2 border-white w-48 cursor-pointer py-2 hover:bg-grey-mid active:bg-grey-mid rounded-3xl'
-                                type='submit'
-                            >
-                                Sign up
-                            </button>
-                        </form>
-                    </div>
+                        </div>
+                        <button
+                            className='border-2 border-white w-48 cursor-pointer py-2 hover:bg-grey-mid active:bg-grey-mid rounded-3xl mt-4'
+                            type='submit'
+                        >
+                            Sign up
+                        </button>
+                    </>
                 );
             }
 
             case 'login': {
                 return (
-                    <div className='font-(family-name:--standard-fonts) flex flex-col gap-1'>
-                        <form className='flex flex-col gap-1'>
-                            <label htmlFor='username'>Username</label>
+                    <>
+                        <div>
+                            <label htmlFor='username' className='block mb-1'>
+                                Username
+                            </label>
                             <input
                                 className='bg-white text-black rounded-xl pl-2 py-1'
                                 id='username'
                             />
-                            <label htmlFor='username'>Password</label>
+                        </div>
+                        <div>
+                            <label htmlFor='username' className='block mb-1'>
+                                Password
+                            </label>
                             <input
                                 className='bg-white text-black rounded-xl pl-2 py-1'
                                 id='password'
                                 type='password'
                             />
-                            <button
-                                className='border-2 border-white w-48 cursor-pointer py-2 hover:bg-grey-mid active:bg-grey-mid rounded-3xl'
-                                type='submit'
-                            >
-                                Log in
-                            </button>
-                        </form>
-                    </div>
+                        </div>
+                        <button
+                            className='border-2 border-white w-48 cursor-pointer py-2 hover:bg-grey-mid active:bg-grey-mid rounded-3xl mt-4'
+                            type='submit'
+                        >
+                            Log in
+                        </button>
+                    </>
                 );
             }
         }
@@ -92,12 +88,51 @@ export default function Login() {
 
     return (
         <div className='font-(family-name:--title-fonts) w-svw h-svh min-h-fit bg-grey-very-dark text-white flex flex-col items-center'>
-            <h1 className='text-5xl font-extrabold text-pink-bright'>
+            <h1 className='text-5xl font-extrabold text-pink-bright mt-8'>
                 DOUBLETS
             </h1>
-            <div className='flex flex-col justify-between items-center gap-4'>
-                <h2>Log in or create an account</h2>
-                {getMainSection()}
+            <div className='flex flex-col items-center'>
+                <div
+                    className={clsx(
+                        'flex flex-col justify-between items-center gap-4 p-8',
+                        type === 'empty' ? '' : 'border-b-2 border-b-white'
+                    )}
+                >
+                    <h2>Log in or create an account</h2>
+                    <button
+                        className={clsx(
+                            'font-(family-name:--standard-fonts) border-2 border-white w-48 cursor-pointer py-2 rounded-3xl',
+                            type === 'login'
+                                ? 'bg-pink-bright'
+                                : 'hover:bg-grey-mid active:bg-grey-mid'
+                        )}
+                        type='button'
+                        onClick={() => {
+                            setType('login');
+                        }}
+                    >
+                        Existing user
+                    </button>
+                    <button
+                        className={clsx(
+                            'font-(family-name:--standard-fonts) border-2 border-white w-48 cursor-pointer py-2 rounded-3xl',
+                            type === 'signup'
+                                ? 'bg-pink-bright'
+                                : 'hover:bg-grey-mid active:bg-grey-mid'
+                        )}
+                        type='button'
+                        onClick={() => {
+                            setType('signup');
+                        }}
+                    >
+                        New user
+                    </button>
+                </div>
+                <div className='font-(family-name:--standard-fonts) my-8 grow'>
+                    <form className='flex flex-col gap-4'>
+                        {getMainSection()}
+                    </form>
+                </div>
             </div>
         </div>
     );
