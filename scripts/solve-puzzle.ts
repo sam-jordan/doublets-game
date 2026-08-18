5861af0be0217c6d13ce43d2773b88d42a773076
sam-jordan <samjjordan24@gmail.com>
2026-08-18 18:05:38 +0300

    solve-puzzle improvements

--- scripts/solve-puzzle.ts
+++ scripts/solve-puzzle.ts
@@ -1,4 +1,8 @@
+#!/usr/bin/env node
+
 import { styleText } from 'node:util';
+import process from 'node:process';
+import { input } from '@inquirer/prompts';
 import { findLinkedWords } from './generate-puzzles.js';
 
 function solvePuzzle(puzzle: { startWord: string; endWord: string }) {
@@ -14,6 +18,10 @@ function solvePuzzle(puzzle: { startWord: string; endWord: string }) {
 
         if (word === puzzle.endWord) {
             chains.push(nextChain);
+        }
+
+        if (chains.length > 50) {
+            return;
         }
 
         for (const linkedWord of wordLinkMapping.get(word)!) {
@@ -45,13 +53,40 @@ function solvePuzzle(puzzle: { startWord: string; endWord: string }) {
         );
     }
 
-    // TODO - handle when no solutions shorter than 20 found
-    chains.sort((a, b) => b.length - a.length);
-    console.log(styleText('green', `Solutions found: ${chains.length}`));
-    console.log(
-        styleText('magenta', `Shortest solution: ${chains[0].join(' -> ')}`)
-    );
+    if (chains.length > 0) {
+        chains.sort((a, b) => b.length - a.length);
+        console.log(styleText('green', `Solutions found: ${chains.length}`));
+        console.log(
+            styleText('magenta', `Shortest solution: ${chains[0].join(' -> ')}`)
+        );
+    } else {
+        console.log(
+            styleText(
+                'red',
+                'Make sure the start and end words have been entered correctly'
+            )
+        );
+    }
 }
 
-// TODO - use inquirer to actually prompt for the puzzle
-solvePuzzle({ startWord: 'BEACH', endWord: 'HOUSE' });
+try {
+    const start = await input({
+        message: styleText('magenta', 'Enter the start word: '),
+    });
+    const end = await input({
+        message: styleText('magenta', 'Enter the end word: '),
+    });
+
+    solvePuzzle({ startWord: start.toUpperCase(), endWord: end.toUpperCase() });
+} catch (error) {
+    if (error instanceof Error) {
+        console.error(
+            styleText(
+                'red',
+                `An error occurred when solving this puzzle: ${error.message}`
+            )
+        );
+    }
+
+    process.exit(1);
+}
