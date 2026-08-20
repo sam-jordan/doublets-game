19fd171e29fe9b03fa1aef5479c445473d7d10c3
sam-jordan <samjjordan24@gmail.com>
2026-08-20 15:39:26 +0300

    Format deploy.yml, add npm ci step

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 const lintstagedConfig = {
     '*.{js,ts,tsx}': ['xo --fix', 'prettier --write'],
     'package.json': ['prettier --write', 'sort-package-json'],
+    '*.yml': ['prettier --write'],
 };
 
 export default lintstagedConfig;
