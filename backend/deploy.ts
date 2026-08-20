28a36133e56c46ee441fea980652a9300b357893
sam-jordan <samjjordan24@gmail.com>
2026-08-20 17:47:27 +0300

    Allow workflow to deploy using OIDC

--- backend/deploy.ts
+++ backend/deploy.ts
@@ -3,7 +3,6 @@ import * as cdk from 'aws-cdk-lib';
 import { Stack } from './stack.js';
 import { environment } from './environment.js';
 
-process.loadEnvFile('./backend/.env');
 const env = environment.parse(process.env);
 
 const app = new cdk.App();
