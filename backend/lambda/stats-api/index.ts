4edd033dc7e8468f03913492a8bffec5c04ca8a8
sam-jordan <samjjordan24@gmail.com>
2026-08-31 01:58:13 +0100

    Add handling for different endpoints in API handler

--- backend/lambda/stats-api/index.ts
+++ backend/lambda/stats-api/index.ts
@@ -4,12 +4,36 @@ import {
 } from 'aws-lambda';
 
 export async function handler(
-    _event: APIGatewayProxyEvent,
+    event: APIGatewayProxyEvent,
     _context: unknown
 ): Promise<APIGatewayProxyResult> {
-    return {
-        statusCode: 200,
-        headers: {},
-        body: 'Hello, world!',
-    };
+    switch (event.resource) {
+        case '/game/{user}/attempted': {
+            return {
+                statusCode: 200,
+                body: 'Hello, world!',
+            };
+        }
+
+        case '/game/{user}/solved': {
+            return {
+                statusCode: 200,
+                body: 'Hello, world!',
+            };
+        }
+
+        case '/stats/{user}': {
+            return {
+                statusCode: 200,
+                body: 'Hello, world!',
+            };
+        }
+
+        default: {
+            return {
+                statusCode: 404,
+                body: `Bad Request: ${event.resource}`,
+            };
+        }
+    }
 }
