0ac3d23ec3ab5e68b06f6324493646d837150efe
sam-jordan <samjjordan24@gmail.com>
2026-09-04 05:49:08 +0100

    safeParse import.meta.env

--- frontend/src/logic/configure-amplify.ts
+++ frontend/src/logic/configure-amplify.ts
@@ -2,21 +2,25 @@ import { Amplify } from 'aws-amplify';
 import { viteEnvironment } from './types';
 
 export default function configureAmplify() {
-    const env = viteEnvironment.parse(import.meta.env);
+    const env = viteEnvironment.safeParse(import.meta.env);
 
-    Amplify.configure({
-        Auth: {
-            Cognito: {
-                userPoolId: env.VITE_USER_POOL_ID,
-                userPoolClientId: env.VITE_USER_POOL_CLIENT_ID,
-                passwordFormat: {
-                    minLength: 8,
-                    requireLowercase: true,
-                    requireUppercase: true,
-                    requireNumbers: true,
-                    requireSpecialCharacters: true,
+    if (env.success) {
+        Amplify.configure({
+            Auth: {
+                Cognito: {
+                    userPoolId: env.data.VITE_USER_POOL_ID,
+                    userPoolClientId: env.data.VITE_USER_POOL_CLIENT_ID,
+                    passwordFormat: {
+                        minLength: 8,
+                        requireLowercase: true,
+                        requireUppercase: true,
+                        requireNumbers: true,
+                        requireSpecialCharacters: true,
+                    },
                 },
             },
-        },
-    });
+        });
+    } else {
+        console.error(env.error);
+    }
 }
