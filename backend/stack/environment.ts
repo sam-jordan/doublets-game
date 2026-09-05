d52b71613ea6d5a642f260e7cc48d4b4ad04f20a
sam-jordan <samjjordan24@gmail.com>
2026-09-05 16:41:48 +0100

    Re-enable api auth for prod

--- backend/stack/environment.ts
+++ backend/stack/environment.ts
@@ -3,8 +3,8 @@ import z from 'zod';
 export const environment = z.object({
     CERTIFICATE_ARN: z.string(),
     DEPLOY_ENV: z.enum(['dev', 'prod']),
-    // USER_POOL_ID: z.string(),
-    // USER_POOL_CLIENT_ID: z.string(),
+    USER_POOL_ID: z.string(),
+    USER_POOL_CLIENT_ID: z.string(),
     DEV_DOMAIN: z.string(),
 });
 
